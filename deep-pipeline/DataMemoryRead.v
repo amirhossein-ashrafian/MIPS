f199ad84b5aa69be97b7e54ce134461a18a648aa
Kishmiish <sma.smh.2771@gmail.com>
2025-02-01 11:01:16 +0330

    fix(deep-pl): fix store then load hazard

--- deep-pipeline/DataMemoryRead.v
+++ deep-pipeline/DataMemoryRead.v
@@ -1,3 +1,7 @@
+// Memory is not synced between memory read and write
+// possible fix is to use another module to handle read and write
+// or use memseth and memsetb and write to a file
+
 module DataMemoryRead (
     input wire clk,
     input wire [31:0] Address,
--- deep-pipeline/DataMemoryWrite.v
+++ deep-pipeline/DataMemoryWrite.v
@@ -1,10 +1,15 @@
 module DataMemoryWrite (
     input wire clk,
-    input wire [31:0] WriteData, Address
+    input reg [31:0] 
+    input wire [31:0] WriteData, Address, ReadDataAddress,
+    output [31:0] ReadDataOutput,
 );
 
     reg [31:0] memory [0:255];
 
-    always @(posedge clk) memory[Address[31:2]] <= WriteData;
+    always @(posedge clk) begin
+        memory[Address[31:2]] <= WriteData;
+        if(Address == ReadDataAddress) ReadDataOutput <= WriteData;
+    end
     
 endmodule
