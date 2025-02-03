9741207bdfac16b45d5e35a2e1e0089a03964a8a
Kishmiish <sma.smh.2771@gmail.com>
2025-02-03 05:16:28 +0330

    change prediction logic to Gshare

--- Branch Predictor/BranchPredictor.v
+++ Branch Predictor/BranchPredictor.v
@@ -7,10 +7,12 @@ module BranchPredictor (
     output        predict   
 );
 
-    reg [1:0] predictor_table [0:1023]; // جدول 1024 سطری ، هر سطر شامل 2 بیت
+    reg [1:0] predictor_table [0:511]; // جدول 1024 سطری ، هر سطر شامل 2 بیت
+    reg [6:0] GBHR // یک نوع شیفت رجیستر برای ذخیره سازی نتیجه آخرین برنچ
 
-    wire [9:0] index;
-    assign index = PC[9:0]; // انتخاب سطر مورد نظر برای بررسی بر اساس 10 بیت کم ارزش پی سی
+    wire [8:0] index;
+    // انتخاب سطر مورد نظر براساس ۲ بیت کم ارزش پی سی و ایکس اور ۸ بیت پرارزشتر و گلوبال برنچ هیستوری رجیستر
+    assign index = {PC[8:2] ^ GBHR, PC[1:0]}; 
 
     assign predict = (predictor_table[index] >= 2) ? 1'b1 : 1'b0; // خروجی پیشبینی به این صورت که اگر مقدار سطر از 2 بیشتر بود تیکن است
 
@@ -18,6 +20,7 @@ module BranchPredictor (
     // به‌روزرسانی جدول پیش‌بینی و مقداردهی اولیه
     always @(posedge clk or posedge reset) begin
         if (reset) begin
+            GBHR <= 7'd0;
             // مقداردهی اولیه تمامی سطرها به حالت 01 (ضعیف غیرتیرش)
             for (i = 0; i < 1024; i = i + 1) begin
                 predictor_table[i] <= 2'b01;
@@ -32,6 +35,7 @@ module BranchPredictor (
                 if (predictor_table[index] != 2'b00)
                     predictor_table[index] <= predictor_table[index] - 1;
             end
+            GBHR <= {GBHR[5:0], actual};
         end
     end
 
